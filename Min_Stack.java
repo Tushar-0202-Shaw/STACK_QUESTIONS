0b25f7aeeb402e19e49330dc4020167fc0d743ce
Tushar Shaw <146598677+Tushar-0202-Shaw@users.noreply.github.com>
2024-06-28 23:03:23 +0530

    Add files via upload

---
+++ Min_Stack.java
@@ -0,0 +1,36 @@
+package Stack_Interview_Questions;
+import java.util.*;
+class MinStack{
+    Stack<Integer> st = new Stack<>();
+    Stack<Integer> min = new Stack<>();
+    public void push(int val){
+        if(st.size()==0){
+            st.push(val);
+            min.push(val);
+        }
+        else{
+            st.push(val);
+            if(min.peek()<val) min.push(min.peek());
+            else min.push(val);
+        }
+    }
+    public void pop(){
+        st.pop();
+        min.pop();
+    }
+    public int getMin(){
+        return min.peek();
+    }
+}
+public class Min_Stack {
+    public static void main(String[] args) {
+        MinStack m = new MinStack();
+        m.push(7);
+        m.push(8);
+        m.push(5);
+        m.push(6);
+        m.push(3);
+        System.out.println(m.st);
+        System.out.println("Min element : "+m.getMin());
+    }
+}
